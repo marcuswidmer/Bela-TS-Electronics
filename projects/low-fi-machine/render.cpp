009009d3f81ddc1b4143d695cc23169447e4ce9f
Marcus Widmer <marwidme@cisco.com>
2021-03-21 23:54:57 +0100

    Added tape stop and start + some cleanup

--- projects/low-fi-machine/render.cpp
+++ projects/low-fi-machine/render.cpp
@@ -24,31 +24,22 @@ The Bela software is distributed under the GNU Lesser General Public License
 
 #include <Bela.h>
 #include <cmath>
-#include <SampleLoader.h>
-#include <SampleData.h>
+#include <libraries/AudioFile/AudioFile.h>
 #include <libraries/Biquad/Biquad.h>
 
-#define MAX_DELAY 1024
+#define MAX_DELAY (10 * 44100)
 #define NUM_CHANNELS 1    // NUMBER OF CHANNELS IN THE FILE
 
 std::string gFilename = "jazz_lick.wav";
+std::string gTapeStopFilename = "tape_lever.wav";
+std::string gTapeStartFilename = "tape_start.wav";
+std::string gHissFilename = "tape_hiss.wav";
 int gNumFramesInFile;
+int gNumFramesInTapeStopFile;
+int gNumFramesInTapeStartFile;
+int gNumFramesInHissFile;
 
-// Two buffers for each channel:
-// one of them loads the next chunk of audio while the other one is used for playback
-SampleData gSampleBuf[NUM_CHANNELS];
-
-// read pointer relative current buffer (range 0-gNumFramesInFile)
-// initialise at gNumFramesInFile to pre-load second buffer (see render())
-float gReadPtr = gNumFramesInFile;
-// read pointer relative to file, increments by gNumFramesInFile (see fillBuffer())
-int gBufferReadPtr = 0;
-// keeps track of which buffer is currently active (switches between 0 and 1)
-int gActiveBuffer = 0;
-// this variable will let us know if the buffer doesn't manage to load in time
-int gDoneLoadingBuffer = 1;
-
-// Because I destroyed the Bela
+// INPUT MAPPING!!! Wierd because I destroyed the Bela
 int gSensorInput0 = 5;
 int gSensorInput1 = 6;
 int gSensorInput2 = 1;
@@ -63,6 +54,7 @@ float gPhase = 0;
 float gInverseSampleRate;
 
 float amplitude = 0.0f;
+float hissAmplitude = 0.0f;
 float speed = 1.0f;
 
 Biquad lpFilter;    // Biquad low-pass frequency;
@@ -81,45 +73,179 @@ float blend = 1;
 float saturation = 1;
 int delay_write_cnt = 0;
 
-float tapeStop;
-bool tapeStopF = false;
+bool doTapeStop = false;
 bool dipStarted = false;
 int dipCounter = 0;
 float dipper = 0;
-const int dipLength = 70000;
-float dipIncrements[dipLength];
+float dipVolume = 1.0f;
+const int nDipVolumes = 100;
+const int dipLength = 0.6 * 44100;
+const int riseLength = 2 * 44100;
+float dipVolumes[nDipVolumes];
+float dipValues[dipLength];
+int dipLimit = 40000;
+
+bool riseStarted = false;
+int riseCounter = 0;
+float riseVolume = 1.0f;
+float riseVolumes[nDipVolumes];
+float riseValues[riseLength];
+
+int stopSampleCounter = 0;
+float stopSampleOut = 0.0f;
+bool stopSampleStarted = false;
+
+int startSampleCounter = 0;
+float startSampleOut = 0.0f;
+bool startSampleStarted = false;
+
+std::vector<float> gSampleData;
+std::vector<float> gHissData;
+std::vector<float> gTapeStopData;
+std::vector<float> gTapeStartData;
+
+static void tapeStartStop(bool doTapeStop)
+{
+    if (doTapeStop) {
+        if (!dipStarted) {
+            dipCounter = 0;
+        }
+        dipper = dipValues[dipCounter];
+        dipStarted = true;
+
+        if (dipCounter < dipLength - 1) {
+            dipCounter++;
+            if (dipCounter > dipLength - nDipVolumes) {
+                dipVolume = dipVolumes[dipCounter - dipLength + nDipVolumes];
+            }
+        } else {
+            if (!stopSampleStarted) {
+                stopSampleCounter = 0;
+            }
+            stopSampleStarted = true;
+
+            if (stopSampleCounter < gNumFramesInTapeStopFile) {
+                stopSampleOut = gTapeStopData[stopSampleCounter];
+                stopSampleCounter++;
+            } else {
+                stopSampleOut = 0.0f;
+            }
+        }
+        riseStarted = false;
+        startSampleStarted = false;
+    } else {
+        if (!startSampleStarted) {
+            startSampleCounter = 0;
+        }
+
+        startSampleStarted = true;
+        if (startSampleCounter < gNumFramesInTapeStartFile) {
+            startSampleOut = gTapeStartData[startSampleCounter];
+            startSampleCounter++;
+        } else {
+            startSampleOut = 0.0f;
+            if (!riseStarted)
+                riseCounter = 0;
+
+            dipper = riseValues[riseCounter];
+            riseStarted = true;
+
+            if (riseCounter < riseLength - 1) {
+                riseCounter++;
+                if (riseCounter < nDipVolumes) {
+                    dipVolume = riseVolumes[riseCounter];
+                }
+            }
+        }
+
+        dipStarted = false;
+        stopSampleStarted = false;
+    }
+}
+
+static float readAudioTestInput()
+{
+    // Increment read pointer and reset to 0 when end of file is reached
+    static int readCounter;
+    float out = gSampleData[readCounter];
+
+    if (readCounter >= gNumFramesInFile)
+        readCounter = 0;
+
+    return out;
+}
+
+static float tapeHiss(float amplitude)
+{
+    static int hissCounter;
+    float hiss = amplitude * gHissData[hissCounter];
+    hissCounter++;
+    if (hissCounter >= gNumFramesInHissFile) {
+        hissCounter = 0;
+    }
+
+    return hiss;
+}
 
 bool setup(BelaContext *context, void *userData)
 {
-    // getNumFrames() and getSamples() are helper functions for getting data from wav files declared in SampleLoader.h
-    // SampleData is a struct that contains an array of floats and an int declared in SampleData.h
+    Bela_setADCLevel(-6.0); //This used to be -6. So changing to the old value to deal with guitar output level.
+
+    printf("Fs: %f. Buffersize: %d\n", context->audioSampleRate, context->audioFrames);
 
     for (int i = 0; i < dipLength; i++) {
-        dipIncrements[i] = i/((float)dipLength) * 1.0f;
+        float x_dip = (float)i / dipLength * 0.5 * M_PI;
+        dipValues[i] = dipLength * 1.0f * (1.0f - pow(cosf(x_dip),1)) / 2.0f;
     }
 
-    gNumFramesInFile = getNumFrames(gFilename);
+    for (int i = 0; i < riseLength; i++) {
+        float x = (float)i / riseLength * 3.0f;
+        riseValues[i] = riseLength / 4.0f * x * exp(-pow(x, 1.5));
+    }
+    riseValues[riseLength - 2] *= 0.5f;
+    riseValues[riseLength - 1] = 0.0f;
+
+    for (int i = 0; i < nDipVolumes; i++) {
+        dipVolumes[i] = 1.0f - (i+1)/((float)nDipVolumes);
+        riseVolumes[i] = i/((float)nDipVolumes);
+    }
 
     gInverseSampleRate = 1.0 / context->audioSampleRate;
-
-    if(gNumFramesInFile <= 0)
-        return false;
 
     if(context->audioFrames)
         gAudioFramesPerAnalogFrame = context->audioFrames / context->analogFrames;
 
-    for(int ch=0;ch<NUM_CHANNELS;ch++) {
-        gSampleBuf[ch].sampleLen = gNumFramesInFile;
-        gSampleBuf[ch].samples = new float[gNumFramesInFile];
-        if(getSamples(gFilename,gSampleBuf[ch].samples,ch,0,gNumFramesInFile))
-            return false;
 
-    }
+    gNumFramesInFile = AudioFileUtilities::getNumFrames(gFilename);
+    gSampleData = AudioFileUtilities::load(gFilename, gNumFramesInFile, 0)[0];
+
+    gNumFramesInTapeStopFile = AudioFileUtilities::getNumFrames(gTapeStopFilename);
+    gTapeStopData = AudioFileUtilities::load(gTapeStopFilename, gNumFramesInTapeStopFile, 0)[0];
+
+    gNumFramesInTapeStartFile = AudioFileUtilities::getNumFrames(gTapeStartFilename);
+    gTapeStartData = AudioFileUtilities::load(gTapeStartFilename, gNumFramesInTapeStartFile, 0)[0];
+
+    gNumFramesInHissFile = AudioFileUtilities::getNumFrames(gHissFilename);
+    gHissData = AudioFileUtilities::load(gHissFilename, gNumFramesInHissFile, 0)[0];
+
+    BiquadCoeff::Settings lp_s = {
+        .fs = context->audioSampleRate,
+        .type = Biquad::lowpass,
+        .cutoff = 3000.0,  // Cut-off frequency for low-pass filter (Hz)
+        .q = gFilterQ,
+        .peakGainDb = 0};
+
+    BiquadCoeff::Settings hp_s = {
+        .fs = context->audioSampleRate,
+        .type = Biquad::highpass,
+        .cutoff = 600.0, // Cut-off frequency for high-pass filter (Hz)
+        .q = gFilterQ,
+        .peakGainDb = 0};
 
     // Set low pass filter parameters (type, frequency & Q)
-    lpFilter.setup(gLPfreq, context->audioSampleRate, Biquad::lowpass, gFilterQ, 0);
+    lpFilter.setup(lp_s);
     // Set high pass filter parameters (type, frequency & Q)
-    hpFilter.setup(gHPfreq, context->audioSampleRate, Biquad::highpass, gFilterQ, 0);
+    hpFilter.setup(hp_s);
 
     return true;
 }
@@ -127,31 +253,23 @@ bool setup(BelaContext *context, void *userData)
 void render(BelaContext *context, void *userData)
 {
     for(unsigned int n = 0; n < context->audioFrames; n++) {
-
         if(gAudioFramesPerAnalogFrame && !(n % gAudioFramesPerAnalogFrame)) {
             amplitude = map(analogRead(context, n/gAudioFramesPerAnalogFrame, gSensorInput0), 0, 1, 0, 1);
-            speed = map(analogRead(context, n/gAudioFramesPerAnalogFrame, gSensorInput1), 0, 1, 0, 2);
+            hissAmplitude = map(analogRead(context, n/gAudioFramesPerAnalogFrame, gSensorInput1), 0, 1, 0, 10);
             vibDepth = map(analogRead(context, n/gAudioFramesPerAnalogFrame, gSensorInput2), 0, 1, 0, 200);
             vibSpeed = map(analogRead(context, n/gAudioFramesPerAnalogFrame, gSensorInput3), 0, 1, 0, 20);
             blend = map(analogRead(context, n/gAudioFramesPerAnalogFrame, gSensorInput4), 0, 1, 0, 1.3);
-            saturation = map(analogRead(context, n/gAudioFramesPerAnalogFrame, gSensorInput5), 0, 1, 0, 50);
-            tapeStop = map(analogRead(context, n/gAudioFramesPerAnalogFrame, gSensorInput6), 0, 1, 0, 1);
-            tapeStopF = tapeStop > 0.5;
+            saturation = map(analogRead(context, n/gAudioFramesPerAnalogFrame, gSensorInput5), 0, 1, 0, 20);
+            doTapeStop = map(analogRead(context, n/gAudioFramesPerAnalogFrame, gSensorInput6), 0, 1, 0, 1) < 0.4;
         }
 
-        // Increment read pointer and reset to 0 when end of file is reached
-        gReadPtr = gReadPtr + speed;
+        //float out = readAudioTestInput();
+        float out = audioRead(context, n, 0);
 
-        if(gReadPtr >= gNumFramesInFile - 1)
-            gReadPtr = 0;
+        // Add tape hiss
+        out = out + tapeHiss(hissAmplitude);
 
-        int gReadPtr_int = gReadPtr;
-        float out_curr = gSampleBuf[0].samples[gReadPtr_int];
-        float out_next = gSampleBuf[0].samples[gReadPtr_int + 1];
-        float frac_playback = gReadPtr - gReadPtr_int;
-        float out = frac_playback * out_next + (1 - frac_playback) * out_curr;
-
-        // float out = audioRead(context, n, 0);
+        // Saturation
         out =  out / (1 + saturation * fabs(out));
 
         // Process input signal with high pass filter
@@ -163,19 +281,7 @@ void render(BelaContext *context, void *userData)
         delayLine[delay_write_cnt] = out;
         float modulator_osc = vibDepth * (1 + sinf(gPhase)) / 2;
 
-        if (tapeStopF) {
-            if (!dipStarted)
-                dipCounter = 0;
-
-            dipper = dipper + dipIncrements[dipCounter];
-            dipStarted = true;
-
-            if (dipCounter < dipLength - 1)
-                dipCounter++;
-        } else {
-            dipper = 0;
-            dipStarted = false;
-        }
+        tapeStartStop(doTapeStop);
 
         int k_0 = modulator_osc + dipper;
         float frac = (modulator_osc + dipper) - k_0;
@@ -193,13 +299,14 @@ void render(BelaContext *context, void *userData)
         if (delay_write_cnt >= MAX_DELAY)
             delay_write_cnt = 0;
 
-        if (abs(amplitude * y) > 1) {
-            printf("Too loud!\n");
+        float yOut = amplitude * (dipVolume * y + stopSampleOut + startSampleOut);
+        if (fabs(yOut) > 3) {
+            printf("Too loud! out: %f\n", yOut);
             abort();
         }
 
-        audioWrite(context, n, 0, amplitude * y);
-        audioWrite(context, n, 1, amplitude * y);
+        audioWrite(context, n, 0, yOut);
+        audioWrite(context, n, 1, yOut);
 
     }
 }
@@ -207,8 +314,4 @@ void render(BelaContext *context, void *userData)
 
 void cleanup(BelaContext *context, void *userData)
 {
-    // Delete the allocated buffers
-    for(int ch=0;ch<NUM_CHANNELS;ch++) {
-        delete[] gSampleBuf[ch].samples;
-    }
 }
